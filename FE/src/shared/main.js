08f20a72a3e60c3be9dbd1c319eb6fa7af50ffa9
H82M19\Student <Student>
2025-10-31 10:39:00 +0700

    add login effect

--- FE/src/shared/main.js
+++ FE/src/shared/main.js
@@ -1,29 +1,34 @@
 import Login from "../components/login/login.js";
 
-function addEffectLogin(triggerElement,renderElement,loginFunction){
+function addEffectLogin(triggerElement,renderElement,loginFunction,loginClass){
     const triggerE = document.querySelector(triggerElement);
     const renderE = document.querySelector(renderElement);
     triggerE.addEventListener('click',(e) => {
+        console.log("add event occurs")
         renderE.innerHTML += loginFunction();
-        console.log("Event occurs !");
+        removeEffectLogin(renderElement,loginClass);
     });
 }
 
 function removeEffectLogin(renderElement,loginClass){
-    const renderE = document.querySelector(renderElement);
-    console.log(renderE);
-    renderE.addEventListener('click',(e) => {
-        console.log(e.currentTarget);
+    const con = document.querySelector('.container');
+    const loginBox = document.querySelector('.login-box');
+    con.addEventListener('click',(e) => {
+        if(loginBox.contains(e.target) == false)
+            con.remove()
     })
 }
 
+// Ý tưởng: 
+// dùng eventTarget để lấy class của thẻ con dc click
+// kiểm tra xem class do981 có phải là con của ligin box hya không 
+
 function createEffectLogin(triggerElement,renderElement,loginFunction,loginClass){
-    addEffectLogin(triggerElement,renderElement,loginFunction);
-    removeEffectLogin(renderElement,loginClass)
+    addEffectLogin(triggerElement,renderElement,loginFunction,loginClass);
 }   
 
 function main(){
-   createEffectLogin('.header__navbar > button','body',Login);
+    createEffectLogin('.header__navbar > button','body',Login,'.login-box'); // true
 }
 main();
 
@@ -43,3 +48,5 @@ main();
 
 // sự khác nhau giữa event.target và evnt.currentTarget 
 // event.target: trả về phần tử thấp nhất mà người dủng thực sự click vào trong cây DOM
+
+
