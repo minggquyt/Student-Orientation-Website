71d175f5eb1836b3c154171fbd76f01b6f7bd3b9
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2025-11-02 10:32:40 +0700

    create server

--- BE/server.js
+++ BE/server.js
@@ -8,15 +8,3 @@ app.get('/',(req,res) => {
 app.listen(3000,() => {
     console.log("Node App is running on port 3000 ");
 }) 
-// Tạo HTTP Server từ instance App 
-// - HTTP Server: lo logic nhận và truyền request 
-// - App: lo logic xử lý request và trả về response
-
-// Phân biệt các khái niệm:
-// - URL: toàn bộ đường dẫn trên trình duyệt bao gồm luôn cả https:// 
-// - Domain: là đoạn đầu tiên sau https://DOMAIN/PATH
-// - Endpoint = HTTP Method + path: là điểm cuối mà client gọi tới để thao tác 
-// VD: GET api/users, POST api/users
-// + về path: api/... : là quy ước khi định nghĩa các routes trả về dữ liệu 
-// -> Giúp phân biệt giữa routes trả về dữ liệu và routes trả về trang HTML 
-
