72b3984aefbb7ecaaa12589d58c78cc73d934fef
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2025-11-04 08:14:25 +0700

    save BE changes

--- BE/server.js
+++ BE/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyparser = require('body-parser')
 const app = express(); 
+
+const port = 3000;
 
 app.get('/',(req,res) => {
     res.send("Hello world");
 })
 
-app.listen(3000,() => {
+// Body-parser middleware - CẤU HÌNH BODY PARSER - TÌM HIỂU DÒNG NÀY
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
+app.post('/saveData',(req,res)=> {
+    console.log("Using body parser: ",req.body.account);
+    res.send("submit okay");
+})
+
+app.listen(port,() => {
     console.log("Node App is running on port 3000 ");
-}) 
+})  
+
+
