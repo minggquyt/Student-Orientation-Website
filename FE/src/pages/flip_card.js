8222e27f75c16e40cfc1be8626b9010c54d3f394
Haslass <lasshas62@gmail.com>
2025-11-15 09:10:54 +0700

    add flip card function again

--- FE/src/pages/flip_card.js
+++ FE/src/pages/flip_card.js
@@ -1,18 +1,12 @@
-//Chọn TẤT CẢ các phần tử .box-content trên trang
 const allBoxes = document.querySelectorAll('.box-content');
 
-//Lặp qua từng 'box' (NodeList)
 allBoxes.forEach(function(box) {
   
-  //Thêm sự kiện click cho MỖI box
   box.addEventListener('click', function() {
     
-    //Tìm .card-container CỦA CHÍNH box này (box vừa được click)
-    //Chứ không phải tìm trên toàn bộ tài liệu
     const cardContainer = box.querySelector('.card-container');
-    
-    //Lật (toggle) chỉ .card-container bên trong box đó
-    if (cardContainer) { // Kiểm tra xem có tìm thấy không
+
+    if (cardContainer) { 
       cardContainer.classList.toggle('is-flipped');
     }
   });
